a0729fd16e8ffaa427efe84012d846e0335dd384
NAVNEET KHARB <145402186+NAVNEET-KHARB@users.noreply.github.com>
2024-04-09 22:49:24 +0530

    Two Sum

--- Algorithms/Easy/001 Two Sum/TwoSum.cpp
+++ Algorithms/Easy/001 Two Sum/TwoSum.cpp
@@ -1,14 +1,19 @@
-// Using brute force
-#include<iostream>
-#include <vector>
+#include <bits/stdc++.h>
 using namespace std;
-class Solution {
+
+// Runtime - 45ms || Memory - 12.72 MB
+class Solution
+{
 public:
-    vector<int> twoSum(vector<int>& nums, int target) {
+    vector<int> twoSum(vector<int> &nums, int target)
+    {
         int n = nums.size();
-        for (int i = 0; i < n - 1; i++) {
-            for (int j = i + 1; j < n; j++) {
-                if (nums[i] + nums[j] == target) {
+        for (int i = 0; i < n - 1; i++)
+        {
+            for (int j = i + 1; j < n; j++)
+            {
+                if (nums[i] + nums[j] == target)
+                {
                     return {i, j};
                 }
             }
@@ -16,3 +21,29 @@ public:
         return {};
     }
 };
+
+// Runtime - 6ms || Memory - 13.61 MB
+class Solution
+{
+public:
+    vector<int> twoSum(vector<int> &nums, int target)
+    {
+        map<int, int> mpp;
+        for (int i = 0; i < nums.size(); i++)
+        {
+            int current = nums[i];
+            int required = target - current;
+            if (mpp.find(required) != mpp.end())
+            {
+                return {mpp[required], i};
+            }
+            mpp[current] = i;
+        }
+        return {};
+    }
+};
+
+int main()
+{
+    return 0;
+}
