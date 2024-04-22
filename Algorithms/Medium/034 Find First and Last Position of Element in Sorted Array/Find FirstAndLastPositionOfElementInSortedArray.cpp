56483b9b7d48b33cb88e8b4504fc40e876308721
NAVNEET KHARB <145402186+NAVNEET-KHARB@users.noreply.github.com>
2024-04-22 11:03:11 +0530

    Search In Rotated Sorted Array 1

--- Algorithms/Medium/034 Find First and Last Position of Element in Sorted Array/Find FirstAndLastPositionOfElementInSortedArray.cpp
+++ Algorithms/Medium/034 Find First and Last Position of Element in Sorted Array/Find FirstAndLastPositionOfElementInSortedArray.cpp
@@ -52,49 +52,44 @@ public:
     }
 };
 
-// Runtime - 4ms || Memory - 16.60 MB
+// Runtime - 6ms || Memory - 15.94 MB
 class Solution
 {
 public:
-    int lowerBound(vector<int> arr, int n, int x)
-    {
-        int low = 0, high = n - 1, ans = n;
-        while (low <= high)
-        {
-            int mid = low + (high - low) / 2;
-            if (arr[mid] >= x)
-            {
-                ans = mid;
-                high = mid - 1;
-            }
-            else
-                low = mid + 1;
-        }
-        return ans;
-    }
-    int upperBound(vector<int> arr, int n, int x)
-    {
-        int low = 0, high = n - 1, ans = n;
-        while (low <= high)
-        {
-            int mid = low + (high - low) / 2;
-            if (arr[mid] > x)
-            {
-                ans = mid;
-                high = mid - 1;
-            }
-            else
-                low = mid + 1;
-        }
-        return ans;
-    }
     vector<int> searchRange(vector<int> &nums, int target)
     {
         int n = nums.size();
-        int lb = lowerBound(nums, n, target);
-        if (lb == n || nums[lb] != target)
-            return {-1, -1};
-        return {lb, upperBound(nums, n, target) - 1};
+        int lowF = 0, highF = n - 1;
+        int lowL = 0, highL = n - 1;
+        int first = -1, last = -1;
+        while (lowF <= highF || lowL <= highL)
+        {
+            int midF = lowF + (highF - lowF) / 2;
+            if (nums[midF] == target)
+            {
+                first = midF;
+                highF = midF - 1;
+            }
+            else if (nums[midF] > target)
+            {
+                highF = midF - 1;
+            }
+            else
+                lowF = midF + 1;
+            int midL = lowL + (highL - lowL) / 2;
+            if (nums[midL] == target)
+            {
+                last = midL;
+                lowL = midL + 1;
+            }
+            else if (nums[midL] > target)
+            {
+                highL = midL - 1;
+            }
+            else
+                lowL = midL + 1;
+        }
+        return {first, last};
     }
 };
 
