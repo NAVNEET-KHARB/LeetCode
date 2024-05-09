fc491b9287925bc8b9c8189157c608557f77244b
NAVNEET KHARB <145402186+NAVNEET-KHARB@users.noreply.github.com>
2024-05-09 19:52:45 +0530

    Middle Of The Linked List

---
+++ Algorithms/Easy/0876 Middle Of The Linked List/MiddleOfTheLinkedList.cpp
@@ -0,0 +1,33 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+// Runtime - 0ms || Memory - 8.61 MB
+struct ListNode
+{
+    int val;
+    ListNode *next;
+    ListNode() : val(0), next(nullptr) {}
+    ListNode(int x) : val(x), next(nullptr) {}
+    ListNode(int x, ListNode *next) : val(x), next(next) {}
+};
+
+class Solution
+{
+public:
+    ListNode *middleNode(ListNode *head)
+    {
+        ListNode *slow = head;
+        ListNode *fast = head;
+        while (fast != NULL && fast->next != NULL)
+        {
+            slow = slow->next;
+            fast = fast->next->next;
+        }
+        return slow;
+    }
+};
+
+int main()
+{
+    return 0;
+}
