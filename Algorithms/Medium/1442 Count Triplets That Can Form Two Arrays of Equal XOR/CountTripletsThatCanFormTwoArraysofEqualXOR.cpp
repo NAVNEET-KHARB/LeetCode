491e76fb84d01ba70670707e24fad93f0d663644
NAVNEET KHARB <145402186+NAVNEET-KHARB@users.noreply.github.com>
2024-05-30 22:27:32 +0530

    Count Triplets That Can Form Two Arrays of Equal XOR

---
+++ Algorithms/Medium/1442 Count Triplets That Can Form Two Arrays of Equal XOR/CountTripletsThatCanFormTwoArraysofEqualXOR.cpp
@@ -0,0 +1,38 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+// Runtime - 0ms || Memory - 9.39 MB
+
+class Solution
+{
+public:
+    int countTriplets(vector<int> &arr)
+    {
+        int n = arr.size() + 1;
+        vector<int> prefix_xor(n, 0);
+        for (int i = 1; i < n; i++)
+        {
+            prefix_xor[i] = prefix_xor[i - 1] ^ arr[i - 1];
+        }
+
+        int res = 0;
+
+        for (int start = 0; start < n; start++)
+        {
+            for (int end = start + 1; end < n; end++)
+            {
+                if (prefix_xor[start] == prefix_xor[end])
+                {
+                    res += end - start - 1;
+                }
+            }
+        }
+
+        return res;
+    }
+};
+
+int main()
+{
+    return 0;
+}
