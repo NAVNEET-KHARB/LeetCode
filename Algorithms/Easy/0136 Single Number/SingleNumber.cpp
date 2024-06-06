f83d61f6130046253e60f6e1f81c6b4bea16c47c
NAVNEET KHARB <145402186+NAVNEET-KHARB@users.noreply.github.com>
2024-06-06 20:27:20 +0530

    Single Number

--- Algorithms/Easy/0136 Single Number/SingleNumber.cpp
+++ Algorithms/Easy/0136 Single Number/SingleNumber.cpp
@@ -1,6 +1,21 @@
 #include <bits/stdc++.h>
 using namespace std;
 
+// Runtime - 11ms || Memory - 19.25 MB
+class Solution
+{
+public:
+    int singleNumber(vector<int> &nums)
+    {
+        int ans = 0;
+        for (int i = 0; i < nums.size(); i++)
+        {
+            ans ^= nums[i];
+        }
+        return ans;
+    }
+};
+
 // Runtime - 18ms || Memory - 19.26 MB
 class Solution
 {
