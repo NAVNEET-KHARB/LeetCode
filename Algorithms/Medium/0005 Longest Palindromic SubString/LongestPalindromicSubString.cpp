4d92ca604176724efeb9d3145dfaa2fe5a97cc1f
NAVNEET KHARB <145402186+NAVNEET-KHARB@users.noreply.github.com>
2024-06-15 22:47:32 +0530

    Longest Palindromic SubString

---
+++ Algorithms/Medium/0005 Longest Palindromic SubString/LongestPalindromicSubString.cpp
@@ -0,0 +1,40 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+// Runtime - 8ms || Memory - 8.18 MB
+class Solution
+{
+public:
+    int expandAroundCenter(const string &s, int left, int right)
+    {
+        while (left >= 0 && right < s.size() && s[left] == s[right])
+        {
+            left--;
+            right++;
+        }
+        return right - left - 1;
+    }
+    string longestPalindrome(string s)
+    {
+        if (s.empty())
+            return "";
+        int start = 0, end = 0;
+        for (int i = 0; i < s.size(); i++)
+        {
+            int len1 = expandAroundCenter(s, i, i);
+            int len2 = expandAroundCenter(s, i, i + 1);
+            int len = max(len1, len2);
+            if (len > end - start)
+            {
+                start = i - (len - 1) / 2;
+                end = i + len / 2;
+            }
+        }
+        return s.substr(start, end - start + 1);
+    }
+};
+
+int main()
+{
+    return 0;
+}
