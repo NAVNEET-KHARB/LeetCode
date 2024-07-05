92720eeb210948f7974990ea5e5a6b9300cfc25d
NAVNEET KHARB <145402186+NAVNEET-KHARB@users.noreply.github.com>
2024-07-05 22:31:13 +0530

    Binary Tree InOrder Traversal

---
+++ Algorithms/Easy/0094 Binary Tree InOrder Travesal/BinaryTreeInOrderTraversal.cpp
@@ -0,0 +1,37 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+// Runtime - 0ms || Memory - 9.78 MB
+struct TreeNode
+{
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+    TreeNode() : val(0), left(nullptr), right(nullptr) {}
+    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+    TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+};
+
+class Solution
+{
+public:
+    void inOrder(TreeNode *root, vector<int> &ans)
+    {
+        if (!root)
+            return;
+        inOrder(root->left, ans);
+        ans.push_back(root->val);
+        inOrder(root->right, ans);
+    }
+    vector<int> inorderTraversal(TreeNode *root)
+    {
+        vector<int> ans;
+        inOrder(root, ans);
+        return ans;
+    }
+};
+
+int main()
+{
+    return 0;
+}
