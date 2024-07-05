1b9d33a5229114153c11b58a264c2750ede4d6dc
NAVNEET KHARB <145402186+NAVNEET-KHARB@users.noreply.github.com>
2024-07-05 21:02:30 +0530

    Maximum Total Importance Of Roads

---
+++ Algorithms/Medium/2285 Maximum Total Importance Of Roads/MaximumTotalImportanceOfRoads.cpp
@@ -0,0 +1,35 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+// Runtime - 222ms || Memory - 117.46 MB
+class Solution
+{
+public:
+    long long maximumImportance(int n, vector<vector<int>> &roads)
+    {
+        vector<int> degree(n, 0);
+        for (const auto &road : roads)
+        {
+            degree[road[0]]++;
+            degree[road[1]]++;
+        }
+        vector<int> cities(n);
+        for (int i = 0; i < n; i++)
+        {
+            cities[i] = i;
+        }
+        sort(cities.begin(), cities.end(), [&](int a, int b)
+             { return degree[a] > degree[b]; });
+        long long totalImportance = 0;
+        for (int i = 0; i < n; i++)
+        {
+            totalImportance += (long long)(n - i) * degree[cities[i]];
+        }
+        return totalImportance;
+    }
+};
+
+int main()
+{
+    return 0;
+}
