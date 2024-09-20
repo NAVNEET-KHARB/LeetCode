fc2de3f7f7597448661ab9bfccd1245d42c9563d
NAVNEET KHARB <145402186+NAVNEET-KHARB@users.noreply.github.com>
2024-09-20 09:57:24 +0530

    Time: 0 ms (100%), Space: 11.6 MB (55.51%) - LeetHub

--- 0214-shortest-palindrome/0214-shortest-palindrome.cpp
+++ 0214-shortest-palindrome/0214-shortest-palindrome.cpp
@@ -1,23 +1,27 @@
 class Solution {
 public:
     string shortestPalindrome(string s) {
-        int length = s.length();
-        if (length == 0) {
-            return s;
-        }
-        int left = 0;
-        for (int right = length - 1; right >= 0; right--) {
-            if (s[right] == s[left]) {
-                left++;
+        string reversedString = string(s.rbegin(), s.rend());
+        string combinedString = s + "#" + reversedString;
+        vector<int> prefixTable = buildPrefixTable(combinedString);
+        int palindromeLength = prefixTable[combinedString.length() - 1];
+        string suffix = reversedString.substr(0, s.length() - palindromeLength);
+        return suffix + s;
+    }
+
+private:
+    vector<int> buildPrefixTable(const string& s) {
+        vector<int> prefixTable(s.length(), 0);
+        int length = 0;
+        for (int i = 1; i < s.length(); i++) {
+            while (length > 0 && s[i] != s[length]) {
+                length = prefixTable[length - 1];
             }
+            if (s[i] == s[length]) {
+                length++;
+            }
+            prefixTable[i] = length;
         }
-        if (left == length) {
-            return s;
-        }
-        string nonPalindromeSuffix = s.substr(left);
-        string reverseSuffix =
-            string(nonPalindromeSuffix.rbegin(), nonPalindromeSuffix.rend());
-        return reverseSuffix + shortestPalindrome(s.substr(0, left)) +
-               nonPalindromeSuffix;
+        return prefixTable;
     }
 };
