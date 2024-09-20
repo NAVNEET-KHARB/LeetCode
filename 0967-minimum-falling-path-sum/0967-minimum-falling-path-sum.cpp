96462479e69f5c13ca3e53a566c683e6bb17f85f
NAVNEET KHARB <145402186+NAVNEET-KHARB@users.noreply.github.com>
2024-09-20 22:53:29 +0530

    Time: 16 ms (22.69%), Space: 13.8 MB (59.89%) - LeetHub

--- 0967-minimum-falling-path-sum/0967-minimum-falling-path-sum.cpp
+++ 0967-minimum-falling-path-sum/0967-minimum-falling-path-sum.cpp
@@ -3,24 +3,26 @@ public:
     int minFallingPathSum(vector<vector<int>>& matrix) {
         int n = matrix.size();
         int m = matrix[0].size();
-        vector<vector<int>> dp(n,vector<int>(m,-1));
+        vector<int> dp(m,-1);
         int ans = INT_MAX;
         for(int j = 0; j<m; j++){
-            dp[0][j] = matrix[0][j];
+            dp[j] = matrix[0][j];
         }
         for(int i = 1; i<n; i++){
+            vector<int> temp(m);
             for(int j = 0; j<m; j++){
                 int st,ld,rd;
                 st = ld = rd = matrix[i][j];
-                st += dp[i-1][j];
-                if(j>0) ld += dp[i-1][j-1];
+                st += dp[j];
+                if(j>0) ld += dp[j-1];
                 else ld += 1e9;
-                if(j+1<m) rd += dp[i-1][j+1];
+                if(j+1<m) rd += dp[j+1];
                 else rd += 1e9;
-                dp[i][j] = min(st,min(ld,rd));
+                temp[j] = min(st,min(ld,rd));
             }
+            dp = temp;
         }
-        for(int j = 0; j<m; j++) ans = min(ans,dp[n-1][j]);
+        for(int j = 0; j<m; j++) ans = min(ans,dp[j]);
         return ans;
     }
 };
