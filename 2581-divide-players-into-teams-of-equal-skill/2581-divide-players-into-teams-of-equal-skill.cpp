1de895f23489924997890e77fa528c93bb924376
NAVNEET KHARB <145402186+NAVNEET-KHARB@users.noreply.github.com>
2024-10-04 15:38:42 +0530

    Time: 69 ms (31.6%), Space: 57.3 MB (14.6%) - LeetHub

--- 2581-divide-players-into-teams-of-equal-skill/2581-divide-players-into-teams-of-equal-skill.cpp
+++ 2581-divide-players-into-teams-of-equal-skill/2581-divide-players-into-teams-of-equal-skill.cpp
@@ -1,15 +1,22 @@
 class Solution {
 public:
     long long dividePlayers(vector<int>& skill) {
-        sort(skill.begin(),skill.end());
         int n = skill.size();
-        int targetSkill = skill[0] + skill[n-1];
-        long long chemistry = 0;
-        for(int i = 0; i<n/2; i++){
-            int currentSkill = skill[i] + skill[n-i-1];
-            if(currentSkill != targetSkill) return -1;
-            chemistry += (long long)skill[i] * (long long)skill[n-i-1];
+        int totalSkill = 0;
+        unordered_map<int,int> skillCount;
+        for(int sk : skill){
+            totalSkill += sk;
+            skillCount[sk]++;
         }
-        return chemistry;
+        if(totalSkill%(n/2) != 0) return -1;
+        int targetSkill = totalSkill/(n/2);
+        long long chemistry = 0;
+        for(int i = 0; i<n; i++){
+            int reqSkill = targetSkill-skill[i];
+            if(!skillCount[reqSkill]) return -1;
+            chemistry += (long long)reqSkill * (long long)skill[i];
+            skillCount[reqSkill]--;
+        }
+        return chemistry/2;
     }
 };
