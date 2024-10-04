78e4598031fc026735341ed1387371c6ad45e1a9
NAVNEET KHARB <145402186+NAVNEET-KHARB@users.noreply.github.com>
2024-10-04 19:21:20 +0530

    Time: 36 ms (54.72%), Space: 20.6 MB (47.2%) - LeetHub

--- 0322-coin-change/0322-coin-change.cpp
+++ 0322-coin-change/0322-coin-change.cpp
@@ -1,20 +1,22 @@
 class Solution {
-    int solve(int ind, int amount, vector<int>& coins, vector<vector<int>>& dp){
-        if(ind == 0){
-            if(amount%coins[ind] == 0) return amount/coins[ind];
-            return 1e9;
-        }
-        if(dp[ind][amount] != 0) return dp[ind][amount];
-        int notTake = solve(ind-1,amount,coins,dp);
-        int take = INT_MAX;
-        if(coins[ind]<=amount) take = 1+solve(ind,amount-coins[ind],coins,dp);
-        return dp[ind][amount] = min(take,notTake);
-    }
 public:
     int coinChange(vector<int>& coins, int amount) {
         int n = coins.size();
-        vector<vector<int>> dp(n,vector<int>(amount+1,0));
-        int result = solve(n - 1, amount, coins, dp);
-        return (result >= 1e9) ? -1 : result;
+        vector<int> dp(amount+1,0);
+        vector<int> temp(amount+1,0);
+        for(int tar = 0; tar<=amount; tar++){
+            if(tar%coins[0] == 0) dp[tar] = tar/coins[0];
+            else dp[tar] = 1e9;
+        }
+        for(int i = 1; i<n; i++){
+            for(int tar = 0; tar<=amount; tar++){
+                int notTake = dp[tar];
+                int take = INT_MAX;
+                if(coins[i]<=tar) take = 1 + temp[tar-coins[i]];
+                temp[tar] = min(notTake,take);
+            }
+            dp = temp;
+        }
+        return dp[amount]>=1e9?-1:dp[amount];
     }
 };
