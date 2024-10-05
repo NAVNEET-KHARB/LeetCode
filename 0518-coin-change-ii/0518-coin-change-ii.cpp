516378e89c34b29dda6bdbbb3b1cf02ee5f8f420
NAVNEET KHARB <145402186+NAVNEET-KHARB@users.noreply.github.com>
2024-10-05 19:21:12 +0530

    Time: 23 ms (19.55%), Space: 44.8 MB (5.97%) - LeetHub

--- 0518-coin-change-ii/0518-coin-change-ii.cpp
+++ 0518-coin-change-ii/0518-coin-change-ii.cpp
@@ -1,18 +1,19 @@
 class Solution {
-    int solve(int ind, int tar, vector<int>& coins, vector<vector<int>>& dp){
-        if(ind == 0){
-            return (tar%coins[0] == 0);
-        }
-        if(dp[ind][tar]!=-1) return dp[ind][tar];
-        int notTake = solve(ind-1,tar,coins,dp);
-        int take = 0;
-        if(coins[ind]<=tar) take = solve(ind,tar-coins[ind],coins,dp);
-        return dp[ind][tar] = take + notTake;
-    }
 public:
     int change(int amount, vector<int>& coins) {
         int n = coins.size();
-        vector<vector<int>> dp(n,vector<int>(amount+1,-1));
-        return solve(n-1,amount,coins,dp);
+        vector<vector<long long>> dp(n,vector<long long>(amount+1,0));
+        for(int tar = 0; tar<=amount; tar++){
+            dp[0][tar] = (tar%coins[0]==0);
+        }
+        for(int ind = 1; ind<n; ind++){
+            for(int tar = 0; tar<=amount; tar++){
+                int notTake = dp[ind-1][tar];
+                int take = 0;
+                if(coins[ind]<=tar) take = dp[ind][tar-coins[ind]];
+                dp[ind][tar] = (long long)take+(long long)notTake;
+            }
+        }
+        return (int)dp[n-1][amount];
     }
 };
