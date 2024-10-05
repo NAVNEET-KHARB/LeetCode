22ff0a6dca41c2a0906328322cf16195a94757cb
NAVNEET KHARB <145402186+NAVNEET-KHARB@users.noreply.github.com>
2024-10-05 19:25:12 +0530

    Time: 15 ms (42.99%), Space: 9.6 MB (82.2%) - LeetHub

--- 0518-coin-change-ii/0518-coin-change-ii.cpp
+++ 0518-coin-change-ii/0518-coin-change-ii.cpp
@@ -2,18 +2,20 @@ class Solution {
 public:
     int change(int amount, vector<int>& coins) {
         int n = coins.size();
-        vector<vector<long long>> dp(n,vector<long long>(amount+1,0));
+        vector<long long> dp(amount+1,0);
+        vector<long long> temp(amount+1,0);
         for(int tar = 0; tar<=amount; tar++){
-            dp[0][tar] = (tar%coins[0]==0);
+            dp[tar] = (tar%coins[0]==0);
         }
         for(int ind = 1; ind<n; ind++){
             for(int tar = 0; tar<=amount; tar++){
-                int notTake = dp[ind-1][tar];
+                int notTake = dp[tar];
                 int take = 0;
-                if(coins[ind]<=tar) take = dp[ind][tar-coins[ind]];
-                dp[ind][tar] = (long long)take+(long long)notTake;
+                if(coins[ind]<=tar) take = temp[tar-coins[ind]];
+                temp[tar] = (long long)take+(long long)notTake;
             }
+            dp = temp;
         }
-        return (int)dp[n-1][amount];
+        return (int)dp[amount];
     }
 };
