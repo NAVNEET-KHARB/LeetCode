1d4a3187c74696ddf2da1e50c97030cb8d65b6b8
NAVNEET KHARB <145402186+NAVNEET-KHARB@users.noreply.github.com>
2024-10-13 14:59:03 +0530

    Time: 10 ms (75.54%), Space: 17.3 MB (79.91%) - LeetHub

--- 0347-top-k-frequent-elements/0347-top-k-frequent-elements.cpp
+++ 0347-top-k-frequent-elements/0347-top-k-frequent-elements.cpp
@@ -1,26 +1,18 @@
 class Solution {
 public:
     vector<int> topKFrequent(vector<int>& nums, int k) {
-        int n = nums.size();
-        sort(nums.begin(),nums.end());
-        priority_queue<pair<int,int>> pq;
-        int num = nums[0];
-        int cnt = 0;
-        for(int i = 0; i<n; i++){
-            if(nums[i] == num) cnt++;
-            else{
-                pq.push({cnt,num});
-                num = nums[i];
-                cnt = 1;
-            }
+        unordered_map<int,int> mpp;
+        for(int num: nums){
+            mpp[num]++;
         }
-        pq.push({cnt,num});
+        priority_queue<pair<int,int>> pq;
+        for(auto it : mpp){
+            pq.push({it.second,it.first});
+        }
         vector<int> ans(k);
         int i = 0;
         while(!pq.empty() && k){
             int num = pq.top().second;
-            cout<<pq.top().first<<"\t";
-            cout<<num<<endl;
             pq.pop();
             ans[i] = num;
             i++;
