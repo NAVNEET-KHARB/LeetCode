e1b3846ac123abcb5257244498cb4e8f2839c7d2
NAVNEET KHARB <145402186+NAVNEET-KHARB@users.noreply.github.com>
2024-10-14 13:43:49 +0530

    Time: 15 ms (92.96%), Space: 26 MB (31.59%) - LeetHub

--- 1250-longest-common-subsequence/1250-longest-common-subsequence.cpp
+++ 1250-longest-common-subsequence/1250-longest-common-subsequence.cpp
@@ -1,14 +1,14 @@
 class Solution {
-    int solve(int ind1, int ind2, string& text1, string& text2, vector<vector<int>>& dp){
-        if(ind1<0 || ind2<0) return 0;
-        if(dp[ind1][ind2] != -1) return dp[ind1][ind2];
-        if(text1[ind1] == text2[ind2]) return dp[ind1][ind2] = 1 + solve(ind1-1,ind2-1,text1, text2, dp);
-        return dp[ind1][ind2] = max(solve(ind1-1,ind2,text1,text2,dp),solve(ind1,ind2-1,text1,text2,dp));
-    }
 public:
     int longestCommonSubsequence(string text1, string text2) {
         int n = text1.length(), m = text2.length();
-        vector<vector<int>> dp(n,vector<int>(m,-1));
-        return solve(n-1,m-1,text1,text2,dp);
+        vector<vector<int>> dp(n+1,vector<int>(m+1,0));
+        for(int i = 1; i<=n; i++){
+            for(int j = 1; j<=m; j++){
+                if(text1[i-1] == text2[j-1]) dp[i][j] = 1 + dp[i-1][j-1];
+                else dp[i][j] = max(dp[i][j-1],dp[i-1][j]);
+            }
+        }
+        return dp[n][m];
     }
 };
