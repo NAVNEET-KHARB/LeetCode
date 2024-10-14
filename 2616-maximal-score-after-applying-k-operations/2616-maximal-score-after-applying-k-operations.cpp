b25ffef8c95126fa2bda9149bd746cfa748f5948
NAVNEET KHARB <145402186+NAVNEET-KHARB@users.noreply.github.com>
2024-10-14 07:53:18 +0530

    Time: 200 ms (42.64%), Space: 79.7 MB (50.62%) - LeetHub

---
+++ 2616-maximal-score-after-applying-k-operations/2616-maximal-score-after-applying-k-operations.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    long long maxKelements(vector<int>& nums, int k) {
+        priority_queue<int> pq;
+        for(int num : nums) pq.push(num);
+        long long ans = 0;
+        while(k){
+            int num = pq.top();
+            pq.pop();
+            ans += num;
+            num%3 == 0?pq.push(num/3):pq.push(num/3 + 1);
+            k--;
+        }
+        return ans;
+    }
+};
