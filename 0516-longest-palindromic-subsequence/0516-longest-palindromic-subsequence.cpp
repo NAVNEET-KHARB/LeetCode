351640f35454f69f77907d54d4326f7e2605b445
NAVNEET KHARB <145402186+NAVNEET-KHARB@users.noreply.github.com>
2024-10-15 10:13:45 +0530

    Time: 54 ms (74.63%), Space: 8.2 MB (97.06%) - LeetHub

--- 0516-longest-palindromic-subsequence/0516-longest-palindromic-subsequence.cpp
+++ 0516-longest-palindromic-subsequence/0516-longest-palindromic-subsequence.cpp
@@ -4,13 +4,15 @@ public:
         int n = s.length();
         string rs(s.begin(),s.end());
         reverse(rs.begin(),rs.end());
-        vector<vector<int>> dp(n+1,vector<int>(n+1,0));
+        vector<int> dp(n+1,0);
+        vector<int> temp(n+1,0);
         for(int i = 1; i<=n; i++){
             for(int j = 1; j<=n; j++){
-                if(s[i-1] == rs[j-1]) dp[i][j] = 1 + dp[i-1][j-1];
-                else dp[i][j] = max(dp[i-1][j],dp[i][j-1]);
+                if(s[i-1] == rs[j-1]) temp[j] = 1 + dp[j-1];
+                else temp[j] = max(dp[j],temp[j-1]);
             }
+            dp = temp;
         }
-        return dp[n][n];
+        return dp[n];
     }
 };
