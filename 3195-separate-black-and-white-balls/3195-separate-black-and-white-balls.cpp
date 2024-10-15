c6bac0c7aa79d38170ee830de27e2e8b5be31137
NAVNEET KHARB <145402186+NAVNEET-KHARB@users.noreply.github.com>
2024-10-15 08:33:34 +0530

    Time: 27 ms (89.47%), Space: 15.6 MB (45.68%) - LeetHub

--- 3195-separate-black-and-white-balls/3195-separate-black-and-white-balls.cpp
+++ 3195-separate-black-and-white-balls/3195-separate-black-and-white-balls.cpp
@@ -1,13 +1,12 @@
 class Solution {
 public:
     long long minimumSteps(string s) {
-        int wPos = 0;
+        int bBall = 0;
         long long swaps = 0;
         for (int i = 0; i < s.length(); i++) {
             if (s[i] == '0') {
-                swaps += i - wPos;
-                wPos++;
-            }
+                swaps += (long long)bBall;
+            }else bBall++;
         }
         return swaps;
     }
