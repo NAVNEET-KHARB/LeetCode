14f1db3f83ac04e2acce324c8133418e827d8f5e
NAVNEET KHARB <145402186+NAVNEET-KHARB@users.noreply.github.com>
2024-10-17 09:23:44 +0530

    Time: 0 ms (100%), Space: 7.5 MB (89.76%) - LeetHub

--- 0670-maximum-swap/0670-maximum-swap.cpp
+++ 0670-maximum-swap/0670-maximum-swap.cpp
@@ -3,14 +3,17 @@ public:
     int maximumSwap(int num) {
         string numStr = to_string(num);
         int n = numStr.size();
-        int maxNum = num;
-        for (int i = 0; i < n; i++) {
-            for (int j = i + 1; j < n; j++) {
-                swap(numStr[i], numStr[j]);
-                maxNum = max(maxNum,stoi(numStr));
-                swap(numStr[i],numStr[j]);
+        vector<int> maxRightIndex(n);
+        maxRightIndex[n - 1] = n - 1;
+        for (int i = n - 2; i >= 0; --i) {
+            maxRightIndex[i] = (numStr[i] > numStr[maxRightIndex[i + 1]])? i: maxRightIndex[i + 1];
+        }
+        for (int i = 0; i < n; ++i) {
+            if (numStr[i] < numStr[maxRightIndex[i]]) {
+                swap(numStr[i],numStr[maxRightIndex[i]]);
+                return stoi(numStr);
             }
         }
-        return maxNum;
+        return num;
     }
 };
