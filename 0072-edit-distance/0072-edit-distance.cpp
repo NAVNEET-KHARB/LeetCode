47f62351a7c54260fb065ed443be8176dab334c4
NAVNEET KHARB <145402186+NAVNEET-KHARB@users.noreply.github.com>
2024-10-17 19:41:21 +0530

    Time: 10 ms (42.36%), Space: 12.8 MB (64.5%) - LeetHub

--- 0072-edit-distance/0072-edit-distance.cpp
+++ 0072-edit-distance/0072-edit-distance.cpp
@@ -1,16 +1,17 @@
 class Solution {
-    int solve(int i, int j, string &word1, string &word2, vector<vector<int>> &dp){
-        if(i==0) return j;
-        if(j==0) return i;
-        if(dp[i][j]!=-1) return dp[i][j];
-        if(word1[i-1]==word2[j-1]) return dp[i][j] = solve(i-1,j-1,word1,word2,dp);
-        return dp[i][j] = 1 + min(solve(i,j-1,word1,word2,dp),min(solve(i-1,j,word1,word2,dp),solve(i-1,j-1,word1,word2,dp)));
-    }
 public:
     int minDistance(string word1, string word2) {
         int n = word1.length();
         int m = word2.length();
-        vector<vector<int>> dp(n+1,vector<int>(m+1,-1));
-        return solve(n,m,word1,word2,dp);
+        vector<vector<int>> dp(n+1,vector<int>(m+1,0));
+        for(int i = 0; i<=n; i++) dp[i][0] = i;
+        for(int j = 0; j<=m; j++) dp[0][j] = j;
+        for(int i = 1; i<=n; i++){
+            for(int j = 1; j<=m; j++){
+                if(word1[i-1] == word2[j-1]) dp[i][j] = dp[i-1][j-1];
+                else dp[i][j] = 1 + min(dp[i][j-1],min(dp[i-1][j],dp[i-1][j-1]));
+            }
+        }
+        return dp[n][m];
     }
 };
