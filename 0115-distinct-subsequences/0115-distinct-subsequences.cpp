236069bf8be601f2e9d5c19dc5d4f8f7abd8ca63
NAVNEET KHARB <145402186+NAVNEET-KHARB@users.noreply.github.com>
2024-10-17 17:01:13 +0530

    Time: 35 ms (23.61%), Space: 43 MB (24.61%) - LeetHub

--- 0115-distinct-subsequences/0115-distinct-subsequences.cpp
+++ 0115-distinct-subsequences/0115-distinct-subsequences.cpp
@@ -1,17 +1,18 @@
 class Solution {
-    long long solve(int i, int j, string &s, string &t, vector<vector<long long>> &dp){
-        if(j==0) return 1;
-        if(i==0) return 0;
-        if(dp[i][j]!=-1) return dp[i][j];
-        if(s[i-1]==t[j-1]) return dp[i][j] = solve(i-1,j-1,s,t,dp) + solve(i-1,j,s,t,dp);
-        return dp[i][j] = solve(i-1,j,s,t,dp);
-    }
 public:
     int numDistinct(string s, string t) {
         int n = s.length();
         int m = t.length();
-        vector<vector<long long>> dp(n+1,vector<long long>(m+1,-1));
-        solve(n,m,s,t,dp);
-        return (dp[n][m]>INT_MAX)?-1:(int)dp[n][m];
+        const long long MOD = 1e9 + 7;
+        vector<vector<long long>> dp(n+1,vector<long long>(m+1,0));
+        for(int i = 0; i<=n; i++) dp[i][0] = 1;
+        for(int j = 1; j<=m; j++) dp[0][j] = 0;
+        for(int i = 1; i<=n; i++){
+            for(int j = 1; j<=m; j++){
+                if(s[i-1]==t[j-1]) dp[i][j] = (dp[i-1][j-1] + dp[i-1][j])%MOD;
+                else dp[i][j] = dp[i-1][j];
+            }
+        }
+        return (int)dp[n][m];
     }
 };
