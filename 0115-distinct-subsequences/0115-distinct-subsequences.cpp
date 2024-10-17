dbafd2e187bdb56bd3cec47fadd19c4eb0b44f7a
NAVNEET KHARB <145402186+NAVNEET-KHARB@users.noreply.github.com>
2024-10-17 17:03:39 +0530

    Time: 16 ms (76.02%), Space: 8.1 MB (86.87%) - LeetHub

--- 0115-distinct-subsequences/0115-distinct-subsequences.cpp
+++ 0115-distinct-subsequences/0115-distinct-subsequences.cpp
@@ -4,15 +4,16 @@ public:
         int n = s.length();
         int m = t.length();
         const long long MOD = 1e9 + 7;
-        vector<vector<long long>> dp(n+1,vector<long long>(m+1,0));
-        for(int i = 0; i<=n; i++) dp[i][0] = 1;
-        for(int j = 1; j<=m; j++) dp[0][j] = 0;
+        vector<long long> dp(m+1,0);
+        vector<long long> temp(m+1,0);
+        dp[0] = temp[0] = 1;
         for(int i = 1; i<=n; i++){
             for(int j = 1; j<=m; j++){
-                if(s[i-1]==t[j-1]) dp[i][j] = (dp[i-1][j-1] + dp[i-1][j])%MOD;
-                else dp[i][j] = dp[i-1][j];
+                if(s[i-1]==t[j-1]) temp[j] = (dp[j-1] + dp[j])%MOD;
+                else temp[j] = dp[j];
             }
+            dp = temp;
         }
-        return (int)dp[n][m];
+        return (int)dp[m];
     }
 };
