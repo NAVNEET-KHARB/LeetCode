6de7f18e83016a635572cf5f959a97a83573a7ef
NAVNEET KHARB <145402186+NAVNEET-KHARB@users.noreply.github.com>
2024-10-17 19:43:57 +0530

    Time: 3 ms (93.44%), Space: 10.3 MB (94.54%) - LeetHub

--- 0072-edit-distance/0072-edit-distance.cpp
+++ 0072-edit-distance/0072-edit-distance.cpp
@@ -3,15 +3,17 @@ public:
     int minDistance(string word1, string word2) {
         int n = word1.length();
         int m = word2.length();
-        vector<vector<int>> dp(n+1,vector<int>(m+1,0));
-        for(int i = 0; i<=n; i++) dp[i][0] = i;
-        for(int j = 0; j<=m; j++) dp[0][j] = j;
+        vector<int> dp(m+1,0);
+        vector<int> temp(m+1,0);
+        for(int j = 0; j<=m; j++) dp[j] = j;
         for(int i = 1; i<=n; i++){
+            temp[0] = i;
             for(int j = 1; j<=m; j++){
-                if(word1[i-1] == word2[j-1]) dp[i][j] = dp[i-1][j-1];
-                else dp[i][j] = 1 + min(dp[i][j-1],min(dp[i-1][j],dp[i-1][j-1]));
+                if(word1[i-1] == word2[j-1]) temp[j] = dp[j-1];
+                else temp[j] = 1 + min(temp[j-1],min(dp[j],dp[j-1]));
             }
+            dp = temp;
         }
-        return dp[n][m];
+        return dp[m];
     }
 };
