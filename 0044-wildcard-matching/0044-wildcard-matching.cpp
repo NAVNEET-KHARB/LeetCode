59b6f875e0f7688a711d375c65a0929ef2508dd7
NAVNEET KHARB <145402186+NAVNEET-KHARB@users.noreply.github.com>
2024-10-18 11:52:34 +0530

    Time: 42 ms (49.48%), Space: 12.1 MB (89.16%) - LeetHub

--- 0044-wildcard-matching/0044-wildcard-matching.cpp
+++ 0044-wildcard-matching/0044-wildcard-matching.cpp
@@ -1,28 +1,34 @@
 class Solution {
+    bool isAllStars(string& S1, int i) {
+        for (int j = 1; j <= i; j++) {
+            if (S1[j - 1] != '*')
+                return false;
+        }
+        return true;
+    }
+
 public:
     bool isMatch(string s, string p) {
         int n = s.length();
         int m = p.length();
-        vector<vector<bool>> dp(n+1,vector<bool>(m+1,false));
-        dp[0][0] = true;
-        for(int i = 1; i<=n; i++) dp[i][0] = false;
-        for(int j = 1; j<=m; j++){
-            bool flag = true;
-            for(int jj = 1;jj<=j; jj++){
-                if(p[jj-1]!='*'){
-                    flag = false;
-                    break;
-                }
-            }
-            dp[0][j] = flag;
+        vector<bool> dp(m + 1, false);
+        vector<bool> temp(m + 1, false);
+        dp[0] = true;
+        for (int j = 1; j <= m; j++) {
+            dp[j] = isAllStars(p, j);
         }
-        for(int i = 1; i<=n; i++){
-            for(int j = 1; j<=m; j++){
-                if(s[i-1]==p[j-1] || p[j-1] == '?') dp[i][j] = dp[i-1][j-1];
-                else if(p[j-1] == '*') dp[i][j] = (dp[i-1][j] || dp[i][j-1]);
-                else dp[i][j] = false;
+        for (int i = 1; i <= n; i++) {
+            temp[0] = false;
+            for (int j = 1; j <= m; j++) {
+                if (s[i - 1] == p[j - 1] || p[j - 1] == '?')
+                    temp[j] = dp[j - 1];
+                else if (p[j - 1] == '*')
+                    temp[j] = (dp[j] || temp[j - 1]);
+                else
+                    temp[j] = false;
             }
+            dp = temp;
         }
-        return dp[n][m];
+        return dp[m];
     }
 };
