94a7fc1c9bbb1e3d76c5429184963bb3bbeb149c
NAVNEET KHARB <145402186+NAVNEET-KHARB@users.noreply.github.com>
2024-10-18 18:10:07 +0530

    Time: 0 ms (100%), Space: 9.9 MB (19.87%) - LeetHub

--- 0010-regular-expression-matching/0010-regular-expression-matching.cpp
+++ 0010-regular-expression-matching/0010-regular-expression-matching.cpp
@@ -1,25 +1,25 @@
 class Solution {
-    int solve(int i, int j, string &s, string &p, vector<vector<int>> &dp){
-        if(i == 0 && j == 0) return 1;
-        if(i>0 && j==0) return 0;
-        if(i == 0 && j>0){
-            if(p[j-1] == '*') return solve(i,j-2,s,p,dp);
-            return 0;
-        }
-        if(dp[i][j]!=-1) return dp[i][j];
-        if(s[i-1] == p[j-1] || p[j-1] == '.') return dp[i][j] = solve(i-1,j-1,s,p,dp);
-        else if(p[j-1] == '*'){
-            bool notTake = solve(i,j-2,s,p,dp);
-            bool take = (s[i-1] == p[j-2] || p[j-2] == '.') && solve(i-1, j, s, p, dp);
-            return dp[i][j] = take||notTake;
-        }
-        return dp[i][j] = 0;
-    }
 public:
     bool isMatch(string s, string p) {
         int n = s.length();
         int m = p.length();
-        vector<vector<int>> dp(n+1,vector<int>(m+1,-1));
-        return solve(n,m,s,p,dp);
+        vector<vector<bool>> dp(n+1,vector<bool>(m+1,false));
+        dp[0][0] = true;
+        for(int i = 1; i<=n; i++) dp[i][0] = false;
+        for(int j = 1; j<=m; j++){
+            if(p[j-1] == '*' && j>=2) dp[0][j] = dp[0][j-2];
+        }
+        for(int i = 1; i<=n; i++){
+            for(int j = 1; j<=m; j++){
+                if(s[i-1] == p[j-1] || p[j-1] == '.') dp[i][j] = dp[i-1][j-1];
+                else if(p[j-1] == '*'){
+                    bool notTake = dp[i][j-2];
+                    bool take = (s[i-1] == p[j-2] || p[j-2] == '.') && dp[i-1][j];
+                    dp[i][j] = take||notTake;
+                }
+                else dp[i][j] = false;
+            }
+        }
+        return dp[n][m];
     }
 };
