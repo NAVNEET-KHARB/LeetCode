c7c88fab711fec73a7d68a238e99d1e8a5e56062
NAVNEET KHARB <145402186+NAVNEET-KHARB@users.noreply.github.com>
2024-10-18 18:16:25 +0530

    Time: 0 ms (100%), Space: 9.2 MB (47.9%) - LeetHub

--- 0010-regular-expression-matching/0010-regular-expression-matching.cpp
+++ 0010-regular-expression-matching/0010-regular-expression-matching.cpp
@@ -3,23 +3,24 @@ public:
     bool isMatch(string s, string p) {
         int n = s.length();
         int m = p.length();
-        vector<vector<bool>> dp(n+1,vector<bool>(m+1,false));
-        dp[0][0] = true;
-        for(int i = 1; i<=n; i++) dp[i][0] = false;
+        vector<bool> dp(m+1,false);
+        vector<bool> temp(m+1,false);
+        dp[0] = true;
         for(int j = 1; j<=m; j++){
-            if(p[j-1] == '*' && j>=2) dp[0][j] = dp[0][j-2];
+            if(p[j-1] == '*' && j>=2) dp[j] = dp[j-2];
         }
         for(int i = 1; i<=n; i++){
             for(int j = 1; j<=m; j++){
-                if(s[i-1] == p[j-1] || p[j-1] == '.') dp[i][j] = dp[i-1][j-1];
+                if(s[i-1] == p[j-1] || p[j-1] == '.') temp[j] = dp[j-1];
                 else if(p[j-1] == '*'){
-                    bool notTake = dp[i][j-2];
-                    bool take = (s[i-1] == p[j-2] || p[j-2] == '.') && dp[i-1][j];
-                    dp[i][j] = take||notTake;
+                    bool notTake = temp[j-2];
+                    bool take = (s[i-1] == p[j-2] || p[j-2] == '.') && dp[j];
+                    temp[j] = take||notTake;
                 }
-                else dp[i][j] = false;
+                else temp[j] = false;
             }
+            dp = temp;
         }
-        return dp[n][m];
+        return dp[m];
     }
 };
