409417ced5c27f1cd31ab064f4790b8bec40b00e
NAVNEET KHARB <145402186+NAVNEET-KHARB@users.noreply.github.com>
2024-10-19 10:41:29 +0530

    Time: 81 ms (69.35%), Space: 93.4 MB (59.52%) - LeetHub

--- 0714-best-time-to-buy-and-sell-stock-with-transaction-fee/0714-best-time-to-buy-and-sell-stock-with-transaction-fee.cpp
+++ 0714-best-time-to-buy-and-sell-stock-with-transaction-fee/0714-best-time-to-buy-and-sell-stock-with-transaction-fee.cpp
@@ -4,11 +4,8 @@ public:
         int n = prices.size();
         vector<vector<int>> dp(n+1,vector<int>(2,0));
         for(int ind = n-1; ind>=0; ind--){
-            for(int buy = 0; buy<=1; buy++){
-                if(buy){
-                    dp[ind][buy] = max(-prices[ind]+dp[ind+1][0],dp[ind+1][1]);
-                }else dp[ind][buy] = max(prices[ind]-fee+dp[ind+1][1],dp[ind+1][0]);
-            }
+            dp[ind][1] = max(-prices[ind]+dp[ind+1][0],dp[ind+1][1]);
+            dp[ind][0] = max(prices[ind]-fee+dp[ind+1][1],dp[ind+1][0]);
         }
         return dp[0][1];
     }
