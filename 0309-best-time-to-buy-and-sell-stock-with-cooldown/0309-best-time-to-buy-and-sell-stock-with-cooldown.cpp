646af58a30fce9bec38739e9096c1b73d40f74f3
NAVNEET KHARB <145402186+NAVNEET-KHARB@users.noreply.github.com>
2024-10-19 10:22:38 +0530

    Time: 1 ms (74%), Space: 14.5 MB (53.13%) - LeetHub

--- 0309-best-time-to-buy-and-sell-stock-with-cooldown/0309-best-time-to-buy-and-sell-stock-with-cooldown.cpp
+++ 0309-best-time-to-buy-and-sell-stock-with-cooldown/0309-best-time-to-buy-and-sell-stock-with-cooldown.cpp
@@ -4,12 +4,8 @@ public:
         int n = prices.size();
         vector<vector<int>> dp(n+2,vector<int>(2,0));
         for(int ind = n-1; ind>=0; ind--){
-            for(int buy = 0; buy<=1; buy++){
-                if(buy){
-                    dp[ind][buy] = max(-prices[ind]+dp[ind+1][0],dp[ind+1][1]);
-                }
-                else dp[ind][buy] = max(prices[ind]+dp[ind+2][1],dp[ind+1][0]);
-            }
+            dp[ind][1] = max(-prices[ind]+dp[ind+1][0],dp[ind+1][1]);
+            dp[ind][0] = max(prices[ind]+dp[ind+2][1],dp[ind+1][0]);
         }
         return dp[0][1];
     }
