8211f5e733d66d10e87a3b61747c1ebbdc0b2c84
NAVNEET KHARB <145402186+NAVNEET-KHARB@users.noreply.github.com>
2025-01-01 21:46:32 +0530

    Time: 1 ms (97.36%), Space: 41.5 MB (49.68%) - LeetHub

--- 1537-maximum-score-after-splitting-a-string/1537-maximum-score-after-splitting-a-string.java
+++ 1537-maximum-score-after-splitting-a-string/1537-maximum-score-after-splitting-a-string.java
@@ -1,16 +1,14 @@
 class Solution {
     public int maxScore(String s) {
         int ones = 0;
-        for(int i = 0; i<s.length(); i++){
-            if(s.charAt(i) == '1') ones++;
-        }
-        int ans = 0;
         int zeroes = 0;
+        int ans = Integer.MIN_VALUE;
         for(int i = 0; i<s.length()-1; i++){
-            if(s.charAt(i) == '1') ones--;
+            if(s.charAt(i) == '1') ones++;
             else zeroes++;
-            ans = Math.max(ans, ones + zeroes);
+            ans = Math.max(ans, zeroes-ones);
         }
-        return ans;
+        if(s.charAt(s.length()-1) == '1') ones++;
+        return ans+ones;
     }
 }
