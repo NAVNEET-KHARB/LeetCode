97bce81010012fcaa7986fc2ca65c295e680f10a
NAVNEET KHARB <145402186+NAVNEET-KHARB@users.noreply.github.com>
2025-01-04 11:14:28 +0530

    Time: 162 ms (76.44%), Space: 45.2 MB (89.79%) - LeetHub

--- 2059-unique-length-3-palindromic-subsequences/2059-unique-length-3-palindromic-subsequences.java
+++ 2059-unique-length-3-palindromic-subsequences/2059-unique-length-3-palindromic-subsequences.java
@@ -1,22 +1,18 @@
 class Solution {
     public int countPalindromicSubsequence(String s) {
-        HashSet<Character> alphas = new HashSet<>();
-        for(char c : s.toCharArray()){
-            alphas.add(c);
+        int[] firstInd = new int[26];
+        int[] lastInd = new int[26];
+        Arrays.fill(firstInd,-1);
+        for(int i = 0; i<s.length(); i++){
+            int alpha = s.charAt(i)-'a';
+            if(firstInd[alpha] == -1) firstInd[alpha] = i;
+            else lastInd[alpha] = i;
         }
         int ans = 0;
-        for(Character alpha : alphas){
-            int firstInd = -1;
-            int lastInd = 0;
-            for(int i = 0; i<s.length(); i++){
-                if(s.charAt(i) == alpha){
-                    if(firstInd == -1) firstInd = i;
-                    else lastInd = i;
-                }
-            }
+        for(int i = 0; i<26; i++){
             HashSet<Character> midChar = new HashSet<>();
-            for(int i = firstInd+1; i<lastInd; i++){
-                midChar.add(s.charAt(i));
+            for(int j = firstInd[i]+1; j<lastInd[i]; j++){
+                midChar.add(s.charAt(j));
             }
             ans += midChar.size();
         }
