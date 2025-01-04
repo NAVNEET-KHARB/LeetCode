0bb13e9b1fc098377ed2a7c933380412831fee10
NAVNEET KHARB <145402186+NAVNEET-KHARB@users.noreply.github.com>
2025-01-04 11:59:06 +0530

    Time: 28 ms (98.54%), Space: 56.9 MB (32.35%) - LeetHub

--- 1966-frequency-of-the-most-frequent-element/1966-frequency-of-the-most-frequent-element.java
+++ 1966-frequency-of-the-most-frequent-element/1966-frequency-of-the-most-frequent-element.java
@@ -3,16 +3,14 @@ class Solution {
         Arrays.sort(nums);
         int left = 0;
         long sum = 0;
-        int ans = 0;
         for(int right = 0; right<nums.length; right++){
             long target = nums[right];
             sum += target;
-            while((right-left+1)*target-sum>k){
+            if((right-left+1)*target-sum>k){
                 sum -= nums[left];
                 left++;
             }
-            ans = Math.max(ans,right-left+1);
         }
-        return ans;
+        return nums.length-left;
     }
 }
