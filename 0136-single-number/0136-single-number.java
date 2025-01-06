7c6ea463530ff3e8c55e6961d2af9a4329541608
NAVNEET KHARB <145402186+NAVNEET-KHARB@users.noreply.github.com>
2025-01-06 21:12:17 +0530

    Time: 1 ms (99.89%), Space: 46.1 MB (52.42%) - LeetHub

--- 0136-single-number/0136-single-number.java
+++ 0136-single-number/0136-single-number.java
@@ -1,10 +1,7 @@
 class Solution {
     public int singleNumber(int[] nums) {
-        Arrays.sort(nums);
-        int n = nums.length;
-        for(int i = 0; i<n-1; i+=2){
-            if(nums[i]!=nums[i+1]) return nums[i];
-        }
-        return nums[n-1];
+        int xorr = 0;
+        for(int num : nums) xorr ^= num;
+        return xorr;
     }
 }
