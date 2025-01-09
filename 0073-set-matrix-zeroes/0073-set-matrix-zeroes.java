f5f0cf866ed414088c51d7b9dba15d72e3693f30
NAVNEET KHARB <145402186+NAVNEET-KHARB@users.noreply.github.com>
2025-01-09 13:40:12 +0530

    Time: 1 ms (73.69%), Space: 45.8 MB (39.73%) - LeetHub

--- 0073-set-matrix-zeroes/0073-set-matrix-zeroes.java
+++ 0073-set-matrix-zeroes/0073-set-matrix-zeroes.java
@@ -2,25 +2,26 @@ class Solution {
     public void setZeroes(int[][] matrix) {
         int m = matrix.length;
         int n = matrix[0].length;
-        HashSet<Integer> rows = new HashSet<>();
-        HashSet<Integer> columns = new HashSet<>();
+        int col0 = 1;
         for(int i = 0; i<m; i++){
             for(int j = 0; j<n; j++){
                 if(matrix[i][j] == 0){
-                    rows.add(i);
-                    columns.add(j);
+                    matrix[i][0] = 0;
+                    if(j == 0) col0 = 0;
+                    else matrix[0][j] = 0;
                 }
             }
         }
-        for(int row : rows){
-            for(int j = 0; j<n; j++){
-                matrix[row][j] = 0;
+        for(int i = 1; i<m; i++){
+            for(int j = 1; j<n; j++){
+                if(matrix[i][0] == 0 || matrix[0][j] == 0) matrix[i][j] = 0;
             }
         }
-        for(int column : columns){
-            for(int i = 0; i<m; i++){
-                matrix[i][column] = 0;
-            }
+        if(matrix[0][0] == 0){
+            for(int j = 0; j<n; j++) matrix[0][j] = 0;
+        }
+        if(col0 == 0){
+            for(int i = 0; i<m; i++) matrix[i][0] = 0;
         }
     }
 }
