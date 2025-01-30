ca7b9eba826a2ae700ad9f39f97fc2ba989a09de
NAVNEET KHARB <145402186+NAVNEET-KHARB@users.noreply.github.com>
2025-01-30 21:20:52 +0530

    Time: 8 ms (95.44%), Space: 50.2 MB (64.82%) - LeetHub

--- 2428-equal-row-and-column-pairs/2428-equal-row-and-column-pairs.java
+++ 2428-equal-row-and-column-pairs/2428-equal-row-and-column-pairs.java
@@ -1,23 +1,15 @@
 class Solution {
     public int equalPairs(int[][] grid) {
-        int ans = 0;
-        int row = 0;
-        while(row<grid.length){
-            HashMap<Integer,Integer> mpp = new HashMap<>();
-            for(int j = 0; j<grid.length; j++) mpp.put(j,grid[row][j]);
-            for(int col = 0; col<grid.length; col++){
-                boolean equal = true;
-                for(int r = 0; r<grid.length; r++){
-                    if(grid[r][col] == mpp.get(r)) continue;
-                    else{
-                        equal = false;
-                        break;
-                    }
-                }
-                if(equal) ans++;
-            }
-            row++;
+        int [][] arr = new int[grid.length][grid.length];
+        int count = 0;
+        for(int i = 0; i < grid.length; i++) {
+            for(int j = 0; j < grid.length; j++) arr[j][i] = grid[i][j];
         }
-        return ans;
+        for(int i = 0; i < grid.length; i++) {
+            for(int j = 0; j < grid.length; j++) {
+                if(Arrays.equals(arr[j],grid[i])) count++;
+            }
+        }
+        return count;
     }
 }
