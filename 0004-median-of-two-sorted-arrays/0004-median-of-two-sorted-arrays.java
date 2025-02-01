6235abc69ad07249a390ae8aa57d22ef875cd489
NAVNEET KHARB <145402186+NAVNEET-KHARB@users.noreply.github.com>
2025-02-01 22:20:59 +0530

    Time: 1 ms (100%), Space: 46.3 MB (26.11%) - LeetHub

--- 0004-median-of-two-sorted-arrays/0004-median-of-two-sorted-arrays.java
+++ 0004-median-of-two-sorted-arrays/0004-median-of-two-sorted-arrays.java
@@ -1,51 +1,26 @@
 class Solution {
     public double findMedianSortedArrays(int[] nums1, int[] nums2) {
-        int n1 = nums1.length, n2 = nums2.length;
-        int i = 0, j = 0;
-        int n = n1+n2;
-        int ind = 0;
-        int ind1 = (n/2)-1, ind2 = (n/2);
-        int el1 = Integer.MIN_VALUE, el2 = Integer.MIN_VALUE;
-        while(i<n1 && j<n2){
-            if(nums1[i]<nums2[j]){
-                if(ind == ind1) el1 = nums1[i];
-                if(ind == ind2){
-                    el2 = nums1[i];
-                    break;
-                }
-                ind++;
-                i++;
-            }else{
-                if(ind == ind1) el1 = nums2[j];
-                if(ind == ind2){
-                    el2 = nums2[j];
-                    break;
-                }
-                ind++;
-                j++;
-            }
+        int n1 = nums1.length;
+        int n2 = nums2.length;
+        if (n1 > n2) return findMedianSortedArrays(nums2, nums1);
+        int low = 0, high = n1;
+        int left = (n1 + n2 + 1) / 2;
+        int n = n1 + n2;
+        while (low <= high) {
+            int mid1 = low + (high - low) / 2;
+            int mid2 = left - mid1;
+            int l1 = Integer.MIN_VALUE, l2 = Integer.MIN_VALUE;
+            int r1 = Integer.MAX_VALUE, r2 = Integer.MAX_VALUE;
+            if (mid1 < n1) r1 = nums1[mid1];
+            if (mid2 < n2) r2 = nums2[mid2];
+            if (mid1 - 1 >= 0) l1 = nums1[mid1 - 1];
+            if (mid2 - 1 >= 0) l2 = nums2[mid2 - 1];
+            if (l1 <= r2 && l2 <= r1) {
+                if (n % 2 == 1) return Math.max(l1, l2);
+                return ((double)Math.max(l1, l2) + (double)Math.min(r1, r2)) / 2.0;
+            } else if (l1 > r2) high = mid1 - 1;
+            else low = mid1 + 1;
         }
-        while(i<n1 && el2==Integer.MIN_VALUE){
-            if(ind == ind1) el1 = nums1[i];
-            if(ind == ind2){
-                el2 = nums1[i];
-                break;
-            }
-            ind++;
-            i++;
-        }
-        while(j<n2 && el2==Integer.MIN_VALUE){
-            if(ind == ind1) el1 = nums2[j];
-            if(ind == ind2){
-                el2 = nums2[j];
-                break;
-            }
-            ind++;
-            j++;
-        }
-        System.out.println(el1);
-        System.out.println(el2);
-        if(n%2 == 1) return el2;
-        return (double)(el1+el2)/2.0;
+        return 0;
     }
 }
