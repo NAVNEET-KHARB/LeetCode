d86ccc3ed6f903161c0916dd13280b40e7379a39
NAVNEET KHARB <145402186+NAVNEET-KHARB@users.noreply.github.com>
2025-02-06 10:47:18 +0530

    Time: 9 ms (95.46%), Space: 57.3 MB (17.26%) - LeetHub

---
+++ 0148-sort-list/0148-sort-list.java
@@ -0,0 +1,48 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+    ListNode findMiddle(ListNode head){
+        if(head == null || head.next == null) return head;
+        ListNode slow = head, fast = head.next;
+        while(fast!=null && fast.next != null){
+            slow = slow.next;
+            fast = fast.next.next;
+        }
+        return slow;
+    }
+    ListNode merge(ListNode list1, ListNode list2){
+        ListNode dummy = new ListNode(-1);
+        ListNode temp = dummy;
+        while(list1!=null && list2!=null){
+            if(list1.val<list2.val){
+                temp.next = list1;
+                temp = list1;
+                list1 = list1.next;
+            }else{
+                temp.next = list2;
+                temp = list2;
+                list2 = list2.next;
+            }
+        }
+        if(list1!=null) temp.next = list1;
+        else temp.next = list2;
+        return dummy.next;
+    }
+    public ListNode sortList(ListNode head) {
+        if(head == null || head.next == null) return head;
+        ListNode middle = findMiddle(head);
+        ListNode leftHead = head, rightHead = middle.next;
+        middle.next = null;
+        leftHead = sortList(leftHead);
+        rightHead = sortList(rightHead);
+        return merge(leftHead,rightHead);
+    }
+}
