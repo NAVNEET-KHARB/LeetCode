a7235c6135e0f1b2f133ef699ccd740a9a58b7d1
NAVNEET KHARB <145402186+NAVNEET-KHARB@users.noreply.github.com>
2025-02-18 15:27:29 +0530

    Time: 42 ms (23.39%), Space: 50.9 MB (75.99%) - LeetHub

--- 1014-k-closest-points-to-origin/1014-k-closest-points-to-origin.java
+++ 1014-k-closest-points-to-origin/1014-k-closest-points-to-origin.java
@@ -17,10 +17,11 @@ class Solution {
         return Math.sqrt((Math.pow(a,2)+Math.pow(b,2)));
     }
     public int[][] kClosest(int[][] points, int k) {
-        PriorityQueue<Pair<Double,int[]>> pq = new PriorityQueue<>(Comparator.comparingDouble(Pair::getKey));
+        PriorityQueue<Pair<Double,int[]>> pq = new PriorityQueue<>((a,b)->Double.compare(b.getKey(), a.getKey()));
         for(int[] point : points){
             double dist = distance(point[0],point[1]);
             pq.add(new Pair(dist,point));
+            if(pq.size()>k) pq.poll();
         }
         int[][] ans = new int[k][2];
         for(int i = 0; i<k; i++){
