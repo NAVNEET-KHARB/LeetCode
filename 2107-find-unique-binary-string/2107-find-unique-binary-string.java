fa544e6b77a5375ec5c158e32cdffbddc8477d55
NAVNEET KHARB <145402186+NAVNEET-KHARB@users.noreply.github.com>
2025-02-20 10:21:55 +0530

    Time: 0 ms (100%), Space: 41.2 MB (98.43%) - LeetHub

--- 2107-find-unique-binary-string/2107-find-unique-binary-string.java
+++ 2107-find-unique-binary-string/2107-find-unique-binary-string.java
@@ -1,21 +1,10 @@
 class Solution {
-    void func(int size, int n, String curr, String[] ans, HashSet<String> set){
-        if(size == n){
-            if(!set.contains(curr)) ans[0] = curr;
-            return;
-        }
-        for(char c = '0'; c<='1'; c++){
-            func(size+1,n,curr+c,ans,set);
-            if(ans[0]!=null) return;
-        }
-    }
     public String findDifferentBinaryString(String[] nums) {
-        HashSet<String> set = new HashSet<>();
-        for(String num : nums) set.add(num);
-        String curr = "";
-        String[] ans = new String[1];
-        int n = nums[0].length(), size = 0;
-        func(size,n,curr,ans,set);
-        return ans[0];
+        StringBuilder ans = new StringBuilder();
+        for(int i = 0; i<nums.length; i++){
+            char curr = nums[i].charAt(i);
+            ans.append(curr=='0'?'1':'0');
+        }
+        return ans.toString();
     }
 }
