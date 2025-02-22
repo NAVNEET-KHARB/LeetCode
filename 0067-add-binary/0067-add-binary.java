782ac2500a4566305bf7e4f3b0f57af6992b7bff
NAVNEET KHARB <145402186+NAVNEET-KHARB@users.noreply.github.com>
2025-02-22 21:26:23 +0530

    Time: 1 ms (99.89%), Space: 42.7 MB (25.67%) - LeetHub

--- 0067-add-binary/0067-add-binary.java
+++ 0067-add-binary/0067-add-binary.java
@@ -1,74 +1,20 @@
 class Solution {
     public String addBinary(String a, String b) {
-        int n = a.length(), m = b.length();
+        char[] charArrA = a.toCharArray();
+        char[] charArrB = b.toCharArray();
         StringBuilder ans = new StringBuilder();
-        int i = n-1, j = m-1;
-        boolean carry = false;
-        while(i>=0 && j>=0){
-            if(a.charAt(i)=='1' && b.charAt(j) == '1'){
-                if(carry){
-                    ans.append('1');
-                }else{
-                    ans.append('0');
-                    carry = true;
-                }
-            }
-            else if(a.charAt(i)=='1' && b.charAt(j) == '0'){
-                if(carry){
-                    ans.append('0');
-                }else{
-                    ans.append('1');
-                }
-            }
-            else if(a.charAt(i)=='0' && b.charAt(j) == '1'){
-                if(carry){
-                    ans.append('0');
-                }else{
-                    ans.append('1');
-                }
-            }
-            else{
-                if(carry){
-                    ans.append('1');
-                    carry = false;
-                }else{
-                    ans.append('0');
-                }
-            }
+        int i = charArrA.length - 1, j = charArrB.length - 1;
+        int carry = 0;
+        while (i >= 0 || j >= 0 || carry > 0) {
+            int aNum = i >= 0 ? charArrA[i] - '0' : 0;
+            int bNum = j >= 0 ? charArrB[j] - '0' : 0;
+            int sum = aNum + bNum + carry;
+            int digit = sum % 2;
+            carry = sum / 2;
             i--;
             j--;
+            ans.append((char)(digit + '0'));
         }
-        while(i>=0){
-            if(a.charAt(i)=='1'){
-                if(carry) ans.append('0');
-                else{
-                    ans.append('1');
-                    carry = false;
-                }
-            }else{
-                if(carry){
-                    ans.append('1');
-                    carry = false;
-                }else ans.append('0');
-            }
-            i--;
-        }
-        while(j>=0){
-            if(b.charAt(j)=='1'){
-                if(carry) ans.append('0');
-                else{
-                    ans.append('1');
-                    carry = false;
-                }
-            }else{
-                if(carry){
-                    ans.append('1');
-                    carry = false;
-                }else ans.append('0');
-            }
-            j--;
-        }
-        if(carry) ans.append('1');
         return ans.reverse().toString();
     }
 }
