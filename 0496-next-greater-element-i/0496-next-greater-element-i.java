e931b41673677260941bb47c8b5795dc2ac09786
NAVNEET KHARB <145402186+NAVNEET-KHARB@users.noreply.github.com>
2025-03-02 22:19:25 +0530

    Time: 3 ms (90.92%), Space: 44.3 MB (31.87%) - LeetHub

--- 0496-next-greater-element-i/0496-next-greater-element-i.java
+++ 0496-next-greater-element-i/0496-next-greater-element-i.java
@@ -3,14 +3,13 @@ class Solution {
         Stack<Integer> st = new Stack<>();
         HashMap<Integer,Integer> mpp = new HashMap<>();
         for(int i = nums2.length-1; i>=0; i--){
-            while(!st.isEmpty() && st.peek()<=nums2[i]) st.pop();
+            while(!st.isEmpty() && nums2[i]>=st.peek()) st.pop();
             if(st.isEmpty()) mpp.put(nums2[i],-1);
             else mpp.put(nums2[i],st.peek());
             st.push(nums2[i]);
         }
-        int len = nums1.length;
-        int[] ans = new int[len];
-        for(int i = 0; i<len; i++){
+        int[] ans = new int[nums1.length];
+        for(int i = 0; i<nums1.length; i++){
             ans[i] = mpp.get(nums1[i]);
         }
         return ans;
