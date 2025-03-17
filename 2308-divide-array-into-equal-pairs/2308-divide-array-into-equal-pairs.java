c8446051126d02547c8d7e386b484d2382268f37
NAVNEET KHARB <145402186+NAVNEET-KHARB@users.noreply.github.com>
2025-03-17 23:12:21 +0530

    Time: 6 ms (45.22%), Space: 44.5 MB (58.82%) - LeetHub

---
+++ 2308-divide-array-into-equal-pairs/2308-divide-array-into-equal-pairs.java
@@ -0,0 +1,10 @@
+class Solution {
+    public boolean divideArray(int[] nums) {
+        HashSet<Integer> set = new HashSet<>();
+        for(int num : nums){
+            if(set.contains(num)) set.remove(num);
+            else set.add(num);
+        }
+        return set.isEmpty();
+    }
+}
