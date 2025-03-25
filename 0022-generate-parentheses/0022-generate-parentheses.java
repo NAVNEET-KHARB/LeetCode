303372a250b4d625a05662ec1bbc1973faed7bbc
NAVNEET KHARB <145402186+NAVNEET-KHARB@users.noreply.github.com>
2025-03-25 22:34:17 +0530

    Time: 1 ms (82.86%), Space: 43.2 MB (59.53%) - LeetHub

--- 0022-generate-parentheses/0022-generate-parentheses.java
+++ 0022-generate-parentheses/0022-generate-parentheses.java
@@ -1,22 +1,19 @@
 class Solution {
-    public void generate(int ind, int n, String brack, int openP, int closeP, List<String> ans){
-        if(openP == n && closeP == n){
-            ans.add(brack);
+    public List<String> generateParenthesis(int n) {
+        List<String> res = new ArrayList<>();
+        dfs(0, 0, "", n, res);
+        return res;        
+    }
+    private void dfs(int openP, int closeP, String s, int n, List<String> res) {
+        if (openP == closeP && openP + closeP == n * 2) {
+            res.add(s);
             return;
         }
-        if(brack.charAt(ind) == '('){
-            if(openP<n) generate(ind+1,n,brack+"(",openP+1,closeP,ans);
-            if(closeP<openP) generate(ind+1,n,brack+")",openP,closeP+1,ans);
-        }else{
-            if(openP<n) generate(ind+1,n,brack+"(",openP+1,closeP,ans);
-            if(closeP<openP) generate(ind+1,n,brack+")",openP,closeP+1,ans);
+        if (openP < n) {
+            dfs(openP + 1, closeP, s + "(", n, res);
         }
-    }
-    public List<String> generateParenthesis(int n) {
-        List<String> ans = new ArrayList<>();
-        String brack = "(";
-        int openP = 1, closeP = 0;
-        generate(0,n,brack,openP,closeP,ans);
-        return ans;
-    }
+        if (closeP < openP) {
+            dfs(openP, closeP + 1, s + ")", n, res);
+        }
+    }    
 }
