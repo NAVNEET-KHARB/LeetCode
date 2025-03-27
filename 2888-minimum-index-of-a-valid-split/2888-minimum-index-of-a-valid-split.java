96f8b1db8f43b206e0b2ff6e02445e03ca5cda45
NAVNEET KHARB <145402186+NAVNEET-KHARB@users.noreply.github.com>
2025-03-27 14:15:57 +0530

    Time: 11 ms (95.51%), Space: 56.6 MB (83.97%) - LeetHub

--- 2888-minimum-index-of-a-valid-split/2888-minimum-index-of-a-valid-split.java
+++ 2888-minimum-index-of-a-valid-split/2888-minimum-index-of-a-valid-split.java
@@ -1,15 +1,18 @@
 class Solution {
     public int minimumIndex(List<Integer> nums) {
-        HashMap<Integer,Integer> mpp = new HashMap<>();
-        int maxFreq = 0, reqNum = -1;
-        for(int num : nums){
-            mpp.put(num,mpp.getOrDefault(num,0)+1);
-            if(mpp.get(num)>maxFreq){
-                maxFreq = mpp.get(num);
-                reqNum = num;
+        int freq = 0, n = nums.size();
+        int maxFreq = 0, reqNum = nums.get(0), cnt = 1;
+        for(int i = 1; i<n;i++){
+            if(nums.get(i) == reqNum) cnt++;
+            else cnt--;
+            if(cnt == 0){
+                reqNum = nums.get(i);
+                cnt = 1;
             }
         }
-        int freq = 0, n = nums.size();
+        for(int num : nums){
+            if(num == reqNum) maxFreq++;
+        }
         for(int i = 0; i<n;i++){
             if(nums.get(i) == reqNum) freq++;
             if(freq*2 > (i+1) && (maxFreq-freq)*2 > (n-i-1)) return i;
