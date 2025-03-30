4dd95d7c72e2d137c1b150408ba460d112abee59
NAVNEET KHARB <145402186+NAVNEET-KHARB@users.noreply.github.com>
2025-03-30 23:00:53 +0530

    Time: 3 ms (77.89%), Space: 43.2 MB (72.13%) - LeetHub

--- 0040-combination-sum-ii/0040-combination-sum-ii.java
+++ 0040-combination-sum-ii/0040-combination-sum-ii.java
@@ -1,24 +1,23 @@
 class Solution {
-    public void combine(int ind, int n, int[] candidates, int target, List<Integer> curr, HashSet<List<Integer>> ans){
-        if(ind == n){
-            if(target == 0) ans.add(new ArrayList<>(curr));
+    public void combine(int ind, int n, int[] candidates, int target, List<Integer> curr, List<List<Integer>> ans){
+        if(target == 0) {
+            if(!ans.contains(curr)) ans.add(new ArrayList<>(curr));
             return;
         }
-        if(candidates[ind]<=target){
-            curr.add(candidates[ind]);
-            combine(ind+1,n,candidates,target-candidates[ind],curr,ans);
-            curr.remove(curr.size()-1);
+        for(int i = ind; i < n; i++){
+            if(i > ind && candidates[i] == candidates[i - 1]) continue;
+            if(candidates[i] > target) break;
+            curr.add(candidates[i]);
+            combine(i + 1, n, candidates, target - candidates[i], curr, ans);
+            curr.remove(curr.size() - 1);
         }
-        combine(ind+1,n,candidates,target,curr,ans);
     }
     public List<List<Integer>> combinationSum2(int[] candidates, int target) {
         Arrays.sort(candidates);
         int n = candidates.length;
-        HashSet<List<Integer>> ans = new HashSet<>();
+        List<List<Integer>> ans = new ArrayList<>();
         List<Integer> curr = new ArrayList<>();
         combine(0,n,candidates,target,curr,ans);
-        List<List<Integer>> ansArr = new ArrayList<>();
-        for(List<Integer> comb : ans) ansArr.add(comb);
-        return ansArr;
+        return ans;
     }
 }
