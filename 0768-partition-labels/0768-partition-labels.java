5ddf4faf9e0cfc2e2ce3a2a77df5a8d204d23233
NAVNEET KHARB <145402186+NAVNEET-KHARB@users.noreply.github.com>
2025-03-30 21:43:08 +0530

    Time: 4 ms (92.83%), Space: 41.5 MB (99.38%) - LeetHub

--- 0768-partition-labels/0768-partition-labels.java
+++ 0768-partition-labels/0768-partition-labels.java
@@ -1,16 +1,15 @@
 class Solution {
     public List<Integer> partitionLabels(String s) {
         int n = s.length();
-        HashMap<Character,Integer> mpp = new HashMap<>();
+        int[] lastOcc = new int[26];
         for(int i = 0; i<n; i++){
-            char c = s.charAt(i);
-            mpp.put(c,i);
+            lastOcc[s.charAt(i)-'a'] = i;
         }
         int ind = 0, partStart = 0, partEnd = 0;
         List<Integer> ans = new ArrayList<>();
         while(ind<n){
             char c = s.charAt(ind);
-            partEnd = Math.max(partEnd,mpp.get(c));
+            partEnd = Math.max(partEnd,lastOcc[c-'a']);
             if(ind == partEnd){
                 ans.add(partEnd-partStart+1);
                 partStart = ind+1;
