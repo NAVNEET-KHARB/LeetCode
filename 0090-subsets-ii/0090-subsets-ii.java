fe6398c20c643903870d73f72455eaa959e37a3a
NAVNEET KHARB <145402186+NAVNEET-KHARB@users.noreply.github.com>
2025-03-31 12:24:46 +0530

    Time: 1 ms (99.87%), Space: 43.5 MB (60.79%) - LeetHub

--- 0090-subsets-ii/0090-subsets-ii.java
+++ 0090-subsets-ii/0090-subsets-ii.java
@@ -1,10 +1,10 @@
 class Solution {
-    public void func(int ind, int n, int[] nums, List<Integer> curr, List<List<Integer>> ans){
+    public void subset(int ind, int n, int[] nums, List<Integer> curr, List<List<Integer>> ans){
         ans.add(new ArrayList<>(curr));
         for(int i = ind; i<n; i++){
             if(i>ind && nums[i] == nums[i-1]) continue;
             curr.add(nums[i]);
-            func(i+1,n,nums,curr,ans);
+            subset(i+1,n,nums,curr,ans);
             curr.remove(curr.size()-1);
         }
     }
@@ -13,7 +13,7 @@ class Solution {
         Arrays.sort(nums);
         List<List<Integer>> ans = new ArrayList<>();
         List<Integer> curr = new ArrayList<>();
-        func(0,n,nums,curr,ans);
+        subset(0,n,nums,curr,ans);
         return ans;
     }
 }
