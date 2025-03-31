91fab22abd8d471a50a2ccbdc45ee3f28a2f1341
NAVNEET KHARB <145402186+NAVNEET-KHARB@users.noreply.github.com>
2025-03-31 10:59:34 +0530

    Time: 32 ms (100%), Space: 63.8 MB (20.15%) - LeetHub

--- 2681-put-marbles-in-bags/2681-put-marbles-in-bags.java
+++ 2681-put-marbles-in-bags/2681-put-marbles-in-bags.java
@@ -1,16 +1,15 @@
 class Solution {
     public long putMarbles(int[] weights, int k) {
         int n = weights.length;
-        PriorityQueue<Integer> minHeap = new PriorityQueue<>();
-        PriorityQueue<Integer> maxHeap = new PriorityQueue<>((a,b)->b-a);
+        int[] splits = new int[n-1];
         for(int i = 0; i<n-1; i++){
-            minHeap.add(weights[i] + weights[i+1]);
-            maxHeap.add(weights[i] + weights[i+1]);
+            splits[i] = weights[i] + weights[i+1];
         }
+        Arrays.sort(splits);
         long maxScore = 0, minScore = 0;
         for(int i = 0; i<k-1; i++){
-            minScore += minHeap.poll();
-            maxScore += maxHeap.poll();
+            minScore += splits[i];
+            maxScore += splits[n-2-i];
         }
         return maxScore-minScore;
     }
