07da1c35e9642c9bce3f9d0dd0532bf8cbab005e
NAVNEET KHARB <145402186+NAVNEET-KHARB@users.noreply.github.com>
2025-04-06 21:55:37 +0530

    Time: 2 ms (12.38%), Space: 48.9 MB (6.63%) - LeetHub

---
+++ 0275-h-index-ii/0275-h-index-ii.java
@@ -0,0 +1,17 @@
+class Solution {
+    public int hIndex(int[] citations) {
+        int papers = citations.length;
+        int[] citationBuckets = new int[papers + 1];
+        for (int citation : citations) {
+            citationBuckets[Math.min(citation, papers)]++;
+        }
+        int cumulativePapers = 0;
+        for (int hIndex = papers; hIndex >= 0; hIndex--) {
+            cumulativePapers += citationBuckets[hIndex];
+            if (cumulativePapers >= hIndex) {
+                return hIndex;
+            }
+        }
+        return 0;
+    }
+}
