e2a5c7393e1019586218786a387e0d69767894fc
NAVNEET KHARB <145402186+NAVNEET-KHARB@users.noreply.github.com>
2025-04-25 23:56:06 +0530

    Time: 0 ms (100%), Space: 41.5 MB (82.15%) - LeetHub

--- 0144-binary-tree-preorder-traversal/0144-binary-tree-preorder-traversal.java
+++ 0144-binary-tree-preorder-traversal/0144-binary-tree-preorder-traversal.java
@@ -15,16 +15,28 @@
  */
 class Solution {
     public List<Integer> preorderTraversal(TreeNode root) {
-        List<Integer> ans = new ArrayList<>();
-        if(root == null) return ans;
-        Stack<TreeNode> st = new Stack<>();
-        st.push(root);
-        while(!st.isEmpty()){
-            root = st.pop();
-            ans.add(root.val);
-            if(root.right != null) st.push(root.right);
-            if(root.left != null) st.push(root.left);
+        // Morris traversal
+        List<Integer> preOrder = new ArrayList<>();
+        TreeNode curr = root;
+        while(curr != null){
+            if(curr.left == null){
+                preOrder.add(curr.val);
+                curr = curr.right;
+            }else{
+                TreeNode next = curr.left;
+                while(next.right != null && next.right != curr){
+                    next = next.right;
+                }
+                if(next.right == null){
+                    next.right = curr;
+                    preOrder.add(curr.val);
+                    curr = curr.left;
+                }else{
+                    next.right = null;
+                    curr = curr.right;
+                }
+            }
         }
-        return ans;
+        return preOrder;
     }
 }
