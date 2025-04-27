688f11c188fc7eafa7386abe3b725e58fa06c451
NAVNEET KHARB <145402186+NAVNEET-KHARB@users.noreply.github.com>
2025-04-27 21:10:38 +0530

    Time: 1 ms (100%), Space: 45.3 MB (29.98%) - LeetHub

--- 3685-count-subarrays-of-length-three-with-a-condition/3685-count-subarrays-of-length-three-with-a-condition.java
+++ 3685-count-subarrays-of-length-three-with-a-condition/3685-count-subarrays-of-length-three-with-a-condition.java
@@ -1,14 +1,8 @@
 class Solution {
     public int countSubarrays(int[] nums) {
         int cnt = 0, n = nums.length;
-        Deque<Integer> dq = new ArrayDeque<>();
-        for(int i = 0; i<n; i++){
-            dq.addLast(nums[i]);
-            if(dq.size() == 3){
-                int sum = dq.peekFirst() + dq.peekLast();
-                dq.pollFirst();
-                if((double)sum == dq.peekFirst()/2.0) cnt++;
-            }
+        for(int i = 1; i<n-1; i++){
+            if(nums[i] == (nums[i-1] + nums[i+1])*2) cnt++;
         }
         return cnt;
     }
