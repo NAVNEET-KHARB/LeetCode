6790f0e16e1fe6478cc6ff3f3c777909274db47c
NAVNEET KHARB <145402186+NAVNEET-KHARB@users.noreply.github.com>
2025-04-29 22:52:38 +0530

    Time: 0 ms (100%), Space: 46.1 MB (47.42%) - LeetHub

---
+++ 0450-delete-node-in-a-bst/0450-delete-node-in-a-bst.java
@@ -0,0 +1,50 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    private TreeNode findLastRight(TreeNode root){
+        if(root.right==null) return root;
+        return findLastRight(root.right);
+    }
+    private TreeNode helper(TreeNode root){
+        if(root.left == null) return root.right;
+        else if(root.left == null) return root.left;
+        else{
+            TreeNode rightChild = root.right;
+            TreeNode lastRight = findLastRight(root.left);
+            lastRight.right = rightChild;
+            return root.left;
+        }
+    }
+    public TreeNode deleteNode(TreeNode root, int key) {
+        if(root == null) return root;
+        if(root.val == key) return helper(root);
+        TreeNode curr = root;
+        while(curr != null){
+            if(curr.val>key){
+                if(curr.left != null && curr.left.val == key){
+                    curr.left = helper(curr.left);
+                    break;
+                }else curr = curr.left;
+            }else{
+                if(curr.right != null && curr.right.val == key){
+                    curr.right = helper(curr.right);
+                    break;
+                }else curr = curr.right;
+            }
+        }
+        return root;
+    }
+}
