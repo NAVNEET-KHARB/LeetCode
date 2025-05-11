bb108dca3172f8230351f9ff104b363290621521
NAVNEET KHARB <145402186+NAVNEET-KHARB@users.noreply.github.com>
2025-05-11 22:12:10 +0530

    Time: 0 ms (100%), Space: 42.4 MB (43.42%) - LeetHub

---
+++ 1293-three-consecutive-odds/1293-three-consecutive-odds.java
@@ -0,0 +1,10 @@
+class Solution {
+    public boolean threeConsecutiveOdds(int[] arr) {
+        int n = arr.length;
+        if(n<3) return false;
+        for(int i = 1; i<n-1; i++){
+            if(arr[i-1]%2==1 && arr[i]%2==1 && arr[i+1]%2==1) return true;
+        }
+        return false;
+    }
+}
