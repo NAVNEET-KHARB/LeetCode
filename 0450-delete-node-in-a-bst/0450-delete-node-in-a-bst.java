76e8797314fcbe7e66448529b97a5b1a2ba8b383
NAVNEET KHARB <145402186+NAVNEET-KHARB@users.noreply.github.com>
2025-05-24 22:34:00 +0530

    Time: 0 ms (100%), Space: 46.3 MB (23.5%) - LeetHub

--- 0450-delete-node-in-a-bst/0450-delete-node-in-a-bst.java
+++ 0450-delete-node-in-a-bst/0450-delete-node-in-a-bst.java
@@ -14,24 +14,24 @@
  * }
  */
 class Solution {
-    private TreeNode lastRight(TreeNode root){
+    public TreeNode lastRight(TreeNode root){
         if(root.right == null) return root;
         return lastRight(root.right);
     }
-    private TreeNode helper(TreeNode root){
+    public TreeNode helper(TreeNode root){
         if(root.left == null) return root.right;
-        else if(root.right == null) return root.left;
-        TreeNode rightChild = root.right;
-        TreeNode lastRightChild = lastRight(root.left);
-        lastRightChild.right = rightChild;
+        if(root.right == null) return root.left;
+        TreeNode right = root.right;
+        TreeNode lastRight = lastRight(root.left);
+        lastRight.right = right;
         return root.left;
     }
     public TreeNode deleteNode(TreeNode root, int key) {
         if(root == null) return root;
         if(root.val == key) return helper(root);
         TreeNode curr = root;
-        while(curr!=null){
-            if(curr.val>key){
+        while(curr != null){
+            if(curr.val > key){
                 if(curr.left != null && curr.left.val == key){
                     curr.left = helper(curr.left);
                     break;
@@ -41,7 +41,6 @@ class Solution {
                     curr.right = helper(curr.right);
                     break;
                 }else curr = curr.right;
-
             }
         }
         return root;
