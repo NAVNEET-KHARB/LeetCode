1ec2fbc582560f2a6c83962a4a007f2b89cc87cd
NAVNEET KHARB <145402186+NAVNEET-KHARB@users.noreply.github.com>
2025-12-06 17:33:08 +0530

    Time: 45 ms (32.1%), Space: 83.9 MB (24.69%) - LeetHub

---
+++ 3578-count-partitions-with-max-min-difference-at-most-k/3578-count-partitions-with-max-min-difference-at-most-k.java
@@ -0,0 +1,45 @@
+class Solution {
+
+    public int countPartitions(int[] nums, int k) {
+        int n = nums.length;
+        long mod = (long) 1e9 + 7;
+        long[] dp = new long[n + 1];
+        long[] prefix = new long[n + 1];
+        Deque<Integer> minQ = new LinkedList<>();
+        Deque<Integer> maxQ = new LinkedList<>();
+
+        dp[0] = 1;
+        prefix[0] = 1;
+        for (int i = 0, j = 0; i < n; i++) {
+            // maintain the maximum value queue
+            while (!maxQ.isEmpty() && nums[maxQ.peekLast()] <= nums[i]) {
+                maxQ.pollLast();
+            }
+            maxQ.offerLast(i);
+            // maintain the minimum value queue
+            while (!minQ.isEmpty() && nums[minQ.peekLast()] >= nums[i]) {
+                minQ.pollLast();
+            }
+            minQ.offerLast(i);
+            // adjust window
+            while (
+                !maxQ.isEmpty() &&
+                !minQ.isEmpty() &&
+                nums[maxQ.peekFirst()] - nums[minQ.peekFirst()] > k
+            ) {
+                if (maxQ.peekFirst() == j) {
+                    maxQ.pollFirst();
+                }
+                if (minQ.peekFirst() == j) {
+                    minQ.pollFirst();
+                }
+                j++;
+            }
+
+            dp[i + 1] = (prefix[i] - (j > 0 ? prefix[j - 1] : 0) + mod) % mod;
+            prefix[i + 1] = (prefix[i] + dp[i + 1]) % mod;
+        }
+
+        return (int) dp[n];
+    }
+}
